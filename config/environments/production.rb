8bc18b18cccc04dfb639ceaa254a6aa593106873
Jesse Wolgamott <jesse@comalproductions.com>
2014-11-18 08:50:24 -0600

    Allow CORS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.middleware.insert_before 0, "Rack::Cors" do
+    allow do
+      origins '*'
+      resource '*', :headers => :any, :methods => [:get, :post, :options]
+    end
 end
